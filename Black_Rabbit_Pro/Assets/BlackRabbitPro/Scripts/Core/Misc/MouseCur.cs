26d3280bc81ca98e1f9dda66a7568877a5057843
Fungus-Light <542461670@qq.com>
2021-01-15 16:24:06 +0800

    add Logo and add useful code

---
+++ Black_Rabbit_Pro/Assets/BlackRabbitPro/Scripts/Core/Misc/MouseCur.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this is a simple tool to open/disable the mouse cur
+[AddComponentMenu("Black-Rabbit/Tools/MouseCur")]
+public class MouseCur : MonoBehaviour
+{
+    public bool isHide=false;
+    void Start()
+    {
+        if (isHide)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        Cursor.visible = !isHide;
+    }
+}
