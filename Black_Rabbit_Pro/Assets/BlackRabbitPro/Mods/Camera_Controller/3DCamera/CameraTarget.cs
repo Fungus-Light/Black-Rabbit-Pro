b524e2464654420a91bc9467ced8ce878cd75499
Fungus-Light <542461670@qq.com>
2021-01-26 14:20:11 +0800

    rerange the dir

--- Black_Rabbit_Pro/Assets/BlackRabbitPro/Mods/Camera_Controller/3DCamera/CameraTarget.cs
+++ Black_Rabbit_Pro/Assets/BlackRabbitPro/Mods/Camera_Controller/3DCamera/CameraTarget.cs
@@ -11,16 +11,4 @@ public class CameraTarget : MonoBehaviour
             Instance=this;
         }
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
