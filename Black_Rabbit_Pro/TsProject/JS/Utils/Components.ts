6de8967e9c90dd15c58bd773965e497a49f0c64d
Fungus-Light <542461670@qq.com>
2021-02-27 11:28:55 +0800

    give up

--- Black_Rabbit_Pro/TsProject/JS/Utils/Components.ts
+++ Black_Rabbit_Pro/TsProject/JS/Utils/Components.ts
@@ -2,22 +2,6 @@ import { UnityEngine } from "csharp"
 import { $typeof } from "puerts"
 import { $, Debug, T } from "./Common"
 
-function $Component<Type extends UnityEngine.Component>(name: string): Type {
-    let trans = $(name)
-    if (trans == null) {
-        Debug.LogError("Can not find " + name)
-        return null
-    } else {
-        
-        let component = trans.GetComponent($typeof(t)) as Type
-        if (component != null) {
-            return component
-        } else {
-            Debug.LogError("Can not find such component in " + name)
-            return null
-        }
-    }
-}
 
 class AudioSource extends UnityEngine.AudioSource { }
 function $AudioSource(name: string): AudioSource {
@@ -28,10 +12,7 @@ function $AudioSource(name: string): AudioSource {
     }
 }
 
-
-
 export {
-    $Component,
     AudioSource,
     $AudioSource,
 
